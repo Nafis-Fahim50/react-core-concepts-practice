04c923f0d45e23b9dd679ed3c6cbb60a5abfcfac
Nafis-Fahim <nafisf96@gmail.com>
2022-09-22 00:03:36 +0600

    check out event listner and useStatehook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>React Similar in look different in Data</h1>
+      <District
+      name='Dhaka'
+      town ='Gulshan'
+      ></District>
+      <District
+      name='Rajshahi'
+      town ='Charghat'
+      ></District>
+      <District
+      name='Chittagong'
+      town ='Potengga'
+      ></District>
     </div>
   );
+}
+
+function District(props){
+  const [power, setPower] = useState(1);
+  const boostPower = () => setPower(power*2);
+  return(
+    <div className='district'>
+      <h3>Distric: {props.name} </h3>
+      <p>Town: {props.town} </p>
+      <h4>Power: {power} </h4>
+      <button onClick={boostPower}>Boost</button>
+    </div>
+  )
 }
 
 export default App;
